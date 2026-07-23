46811dde55137a2bb0d8ea7067a45fc4800f7138
notsuz <ifosuz77@gmail.com>
2026-07-23 15:47:00 +0545

    productionPush

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 echo "Building project..."
+
+
 python3 -m pip install -r requirements.txt
-python3 manage.py makemigrations --noinput
-python3 manage.py migrate --noinput
-python3 manage.py collectstatic --noinput --clear
+
+
+python3 -m django makemigrations --noinput
+python3 -m django migrate --noinput
+python3 -m django collectstatic --noinput --clear
+
+echo "Build complete."
