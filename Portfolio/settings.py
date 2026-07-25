5b985dd93278974f092cbad816ed86ecba60d973
notsuz <ifosuz77@gmail.com>
2026-07-25 11:46:12 +0545

    captionFix

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'whitenoise.runserver_nostatic',
     'cloudinary_storage',
     'cloudinary',
 ]
